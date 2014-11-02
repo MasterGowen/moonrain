0a68088b38c99c08175401e0ba546946fa5d923c
Master Gowen <mastergowen@gmail.com>
2014-11-02 21:21:29 +0500

    Ungerister Taggit from admin.site

--- moonrain/projects/admin.py
+++ moonrain/projects/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Project
 from .forms import ProjectForm
+from taggit.models import Tag
 
 
 class ProjectAdmin(admin.ModelAdmin):
@@ -8,3 +9,4 @@ class ProjectAdmin(admin.ModelAdmin):
     list_display = ('name', 'author', 'date', 'Комментарий', 'users')
 
 admin.site.register(Project, ProjectAdmin)
+admin.site.unregister(Tag)
