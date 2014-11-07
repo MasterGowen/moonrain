ffbe5e87691f973535a1c972d74b6c31d42cb0e4
Master Gowen <mastergowen@gmail.com>
2014-11-07 07:54:32 +0500

    Functionality for top menu buttons

--- moonrain/urls.py
+++ moonrain/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^videos/', vidviews.video_list),
                        url(r'^$', projviews.projects_list_all, name='all_projects'),
+                       url(r'^projects/', projviews.projects_list_all, name='all_projects'),
                        url(r'^projects/([0-9]+)/', projviews.detail),
 
 )
@@ -21,7 +22,7 @@ if settings.DEBUG:
 
 #AA
 urlpatterns += patterns('',
-                        url(r'^login/', 'django.contrib.auth.views.login', { "template_name": "accounts/login.html" }),
+                        url(r'^login/', 'django.contrib.auth.views.login', {"template_name": "accounts/login.html"}),
                         url(r'logout/', 'django.contrib.auth.views.logout')
                         )
 
