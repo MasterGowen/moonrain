b785955ccf68e3dc30508bb235e3788f456db293
Master Gowen <mastergowen@gmail.com>
2014-11-07 12:03:32 +0500

    Add simple videos page

--- moonrain/videos/views.py
+++ moonrain/videos/views.py
@@ -1,16 +1,27 @@
 from django.http import HttpResponse, HttpRequest
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Video
 
 
+@login_required
 def video_list(request):
-    all_videos_names = []
-    if request.user.has_perm('projects.can_view_project'):
-        for video in Video.objects.all():
-            all_videos_names.append(video.comments)
-    else:
-        all_videos_names = '<h1>Access denied</h1>'
-    return HttpResponse(str(all_videos_names))
+
+    videos = Video.objects.filter(author=request.user)
+
+    paginator = Paginator(videos, 10)
+    page = request.GET.get('page')
+
+    try:
+        videos = paginator.page(page)
+    except PageNotAnInteger:
+        videos = paginator.page(1)
+    except EmptyPage:
+        videos = paginator.page(paginator.num_pages)
+
+    return render(request, 'videos/index.html', {'videos': videos, 'pages': range(1, (paginator.num_pages + 1))})
 
 
-def video_detail(request, id):
+def video_detail(request, video_id):
     pass
